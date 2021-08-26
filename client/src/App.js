8e64848d15beb75d058fb861b7e635e5867e8e32
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-26 11:54:50 -0400

    file structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,14 @@
+import React from 'react'
+import NavBar from './components/NavBar'
+
 function App() {
-  return <div className="App"></div>
+  return (
+    <>
+      <div className="App">
+        <NavBar />
+      </div>
+    </>
+  )
 }
 
 export default App
---
+++ client/src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function NavBar() {
+  return (
+    <>
+      <div>hi</div>
+    </>
+  )
+}
+
+export default NavBar
