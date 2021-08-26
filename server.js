fef1958eb938c6181cb0dd931323b02a262f7d3c
Neil Italia <neilitalia1998@gmail.com>
2021-08-26 10:20:42 -0500

    express server

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const logger = require('morgan')
+const db = require('./db')
+// const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.use(cors())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(logger('dev'))
+
+// TODO: Routes
+// app.use('/api', AppRouter)
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.listen(PORT, () => console.log(`Server Running On Port:`, PORT))
