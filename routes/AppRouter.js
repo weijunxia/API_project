e380ccd0e5fcb67281d38182831bc9994376fcbe
Neil Italia <neilitalia1998@gmail.com>
2021-08-26 13:01:10 -0500

    configure getAllPosts API endpoint

--- routes/AppRouter.js
+++ routes/AppRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express')
-const controllers = require('../controllers')
+// const controllers = require('../controllers')
+const postController = require('../controllers/PostControllers')
 const AppRouter = Router()
 
 AppRouter.get('/', (req, res) => res.send())
-AppRouter.post('/posts', controllers.createPost)
-AppRouter.put('/posts', controllers.updatePost)
-AppRouter.delete('/posts', controllers.deletePost)
+AppRouter.get('/posts', postController.getAllPosts)
+// AppRouter.post('/posts', controllers.createPost)
+// AppRouter.put('/posts', controllers.updatePost)
+// AppRouter.delete('/posts', controllers.deletePost)
 
 module.exports = AppRouter
