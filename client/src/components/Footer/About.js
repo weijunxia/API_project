739b34219bbc6d0bc1f7ab396e958a93e4faaa63
msiroilem <twjones86@gmail.com>
2021-08-26 15:27:36 -0400

    added content to About and Careers

--- client/src/components/Footer/About.js
+++ client/src/components/Footer/About.js
@@ -4,7 +4,10 @@ import './about.css'
 function About() {
   return (
     <div>
-      <div></div>
+      <p>
+        At Bloggr, we're creating a space where you can write about whatever is
+        on your mind.
+      </p>
     </div>
   )
 }
