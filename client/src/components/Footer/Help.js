aeb91f147dad5ff84066b64a41e8dcde1be2ad1b
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-27 10:59:43 -0400

    css

--- client/src/components/Footer/Help.js
+++ client/src/components/Footer/Help.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './help.css'
 
 export default function Help() {
   return (
     <div>
-      <h3>Help</h3>
-      <p>We can't help you. No one can.</p>
+      <h3 className="help">Help</h3>
+      <p className="help">We can't help you. No one can.</p>
     </div>
   )
 }
