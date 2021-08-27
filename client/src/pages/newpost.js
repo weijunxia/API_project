eecd6a69ac57a1fa54159e445645004572375312
Wei Jun Xia <weijunx1224@gmail.com>
2021-08-26 20:15:30 -0400

    routing

---
+++ client/src/pages/newpost.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './createpost.css'
+
+const CreatePost = () => {
+  return (
+    <div className="create-post-container">
+      <form className="new-post">
+        <input
+          type="text"
+          placeholder="Post Title"
+          className="post-title"
+        ></input>
+        <textarea
+          type="text"
+          placeholder="Write away"
+          className="post-content"
+        ></textarea>
+        <button type="submit" className="post-submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export default CreatePost
